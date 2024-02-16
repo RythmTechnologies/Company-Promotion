8cf453994a49168fa4297cc13708f6e0140e980b
CoderMungan <codermungan@gmail.com>
2024-02-16 18:13:25 +0300

    feat: add blog & service & testimonials models

--- company/apps/service/models.py
+++ company/apps/service/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from apps.main.mixin import TimeBasedStampModel
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+ICON_CHOICES = (
+        ("1", "TEST"),
+        ("2", "TEST"),
+        ("3", "TEST"),
+        ("4", "TEST"),
+        ("5", "TEST"),
+        ("6", "TEST"),
+        ("7", "TEST")
+    )
+
+class Services(TimeBasedStampModel):
+  title = models.CharField(("Title"), max_length=50)
+  short_content = models.CharField(("Short Content"), max_length=127)
+  content = HTMLField(("Content"))
+  icon = models.CharField(("Icon"),choices=ICON_CHOICES, max_length=150)
+
+  class Meta:
+    verbose_name = "Service"
+    verbose_name_plural = "Services"
+
+  def __str__(self) -> str:
+      return self.title
+
--- company/apps/testimonials/models.py
+++ company/apps/testimonials/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.main.mixin import TimeBasedStampModel
+from django.core.validators import MaxValueValidator
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Testimimonials(TimeBasedStampModel):
+  author = models.CharField(("Author"), max_length=150)
+  title = models.CharField(("Title"), max_length=150)
+  image = models.ImageField(("Image"), upload_to="testimoninials/", height_field=None, width_field=None, max_length=None)
+  star = models.PositiveIntegerField(("Star Point"),default = 1, validator = [MaxValueValidator(5)])
+  content = HTMLField(("Testimonial Content"), max_length = 400)
+
+  class Meta:
+    verbose_name = "Testimonial"
+    verbose_name_plural = "Testimonials"
+
+  def __str__(self) -> str:
+      return self.author
