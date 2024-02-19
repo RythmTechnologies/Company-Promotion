0c473483eb1c68d8c29b99778ad922198e49c678
CoderMungan <codermungan@gmail.com>
2024-02-19 23:14:11 +0300

    feat: add blog and single blog model & views

--- company/apps/blog/admin.py
+++ company/apps/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Blog, Label, Category
 
-# Register your models here.
+admin.site.register(Blog)
+admin.site.register(Category)
+admin.site.register(Label)
